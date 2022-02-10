ac4046ab6ee9965e739c57b0bb5c902b7301ea13
jsnwang <jwang11211@gmail.com>
2022-02-10 17:29:19 -0500

    Created DetailsFragment and DetailsViewModel

--- feature_details/src/main/java/com/example/feature_details/view/DetailsFragment.kt
+++ feature_details/src/main/java/com/example/feature_details/view/DetailsFragment.kt
@@ -1,22 +1,25 @@
-package com.example.feature_edit.view
+package com.example.feature_details.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.feature_edit.databinding.FragmentEditBinding
+import androidx.fragment.app.viewModels
+import com.example.feature_details.databinding.FragmentEditBinding
+import com.example.feature_details.viewmodel.DetailsViewModel
 
-class EditFragment : Fragment() {
+class DetailsFragment : Fragment() {
 
-    private var _binding: FragmentEditBinding? = null
-    private val binding get() = _binding
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModels<DetailsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = FragmentEditBinding.inflate(inflater, container, false).also {
+    ) = FragmentDetailsBinding.inflate(inflater, container, false).also {
         _binding = it
     }.root
 
---
+++ feature_details/src/main/java/com/example/feature_details/viewmodel/DetailsViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.feature_details.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.model_todo.TodoRepo
+import com.example.model_todo.local.TodoDatabase
+
+class DetailsViewModel (app: Application) : AndroidViewModel(app) {
+
+    private val todoRepo by lazy {
+        TodoRepo(TodoDatabase.getDatabase(app, viewModelScope).todoDao())
+    }
+
+    fun showDetails(){
+
+    }
+}
