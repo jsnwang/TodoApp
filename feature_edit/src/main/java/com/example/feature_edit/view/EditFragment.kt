1e0318f051b57bc3f6631999901931691594be72
noahcams <noahcmr@gmail.com>
2022-02-11 15:49:44 -0500

    Added the by viewModels in EditFragment

--- feature_edit/src/main/java/com/example/feature_edit/view/EditFragment.kt
+++ feature_edit/src/main/java/com/example/feature_edit/view/EditFragment.kt
@@ -8,13 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.feature_edit.databinding.FragmentEditBinding
+import com.example.feature_edit.viewmodel.EditViewModel
 
 class EditFragment : Fragment() {
 
     private var _binding: FragmentEditBinding? = null
-    private val binding get() = _binding
+    private val binding get() = _binding!!
+
+    private val viewModel by viewModels<EditViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
